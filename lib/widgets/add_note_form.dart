658d538a9d18793b48507c136c6fbda32d96883d
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-11-07 10:16:34 +0200

    refactor keyboard  in bottom model sheet

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -48,23 +48,27 @@ class _AddNoteFormState extends State<AddNoteForm> {
             height: 32,
           ),
           BlocBuilder<AddNoteCubit, AddNoteState>(
+            //to make button listen to any change to changes its state
             builder: (context, state) {
-              return CustomAddNoteButton(
-                isLoading: state is AddNoteLoading ? true : false,
-                onTap: () {
-                  if (formKey.currentState!.validate()) {
-                    formKey.currentState!.save();
-                    var noteModel = NoteModel(
-                        title: title!,
-                        subTitle: subTitle!,
-                        date: DateTime.now().toString(),
-                        color: Colors.blue.value);
-                    BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
-                  } else {
-                    autovalidateMode = AutovalidateMode.always;
-                    setState(() {});
-                  }
-                },
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: CustomAddNoteButton(
+                  isLoading: state is AddNoteLoading ? true : false,
+                  onTap: () {
+                    if (formKey.currentState!.validate()) {
+                      formKey.currentState!.save();
+                      var noteModel = NoteModel(
+                          title: title!,
+                          subTitle: subTitle!,
+                          date: DateTime.now().toString(),
+                          color: Colors.blue.value);
+                      BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
+                    } else {
+                      autovalidateMode = AutovalidateMode.always;
+                      setState(() {});
+                    }
+                  },
+                ),
               );
             },
           ),
