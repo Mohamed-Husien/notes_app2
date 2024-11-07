5fd51a63670f71106ed33558d94426f4c595b751
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-11-07 11:18:39 +0200

    create fetchAllNotes method and provide cubit to notes view

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/constants.dart';
 import 'package:note_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  fetchAllNotes() async {
+    try {
+      var noteBox = Hive.box<NoteModel>(kNodeBox); //generic concept
+
+      emit(NotesSuccess(notes: noteBox.values.toList()));
+    } catch (e) {
+      emit(
+        NotesFailure(errMessage: e.toString()),
+      );
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/constants.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:note_app/widgets/add_note_bottom_sheet.dart';
 import 'package:note_app/widgets/notes_view_body.dart';
 
@@ -8,27 +10,31 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: kPrimaryColor,
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            context: context,
-            builder: (context) {
-              return const AddNoteBottomSheet();
-            },
-          );
-        },
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
+    return BlocProvider(
+      create: (context) =>
+          NotesCubit(), //provide notes cubit to notws view to display all notes in Hive
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: kPrimaryColor,
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              },
+            );
+          },
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
         ),
+        body: const NotesViewBody(),
       ),
-      body: const NotesViewBody(),
     );
   }
 }
