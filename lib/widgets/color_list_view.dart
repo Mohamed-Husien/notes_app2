2dd3ca382a632d9491afafd1661282b19d66e3dc
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-11-09 10:37:19 +0200

    colors list view part tow

--- lib/widgets/color_list_view.dart
+++ lib/widgets/color_list_view.dart
@@ -1,20 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/color_item.dart';
 
-class ColorListView extends StatelessWidget {
+class ColorListView extends StatefulWidget {
   const ColorListView({super.key});
 
+  @override
+  State<ColorListView> createState() => _ColorListViewState();
+}
+
+class _ColorListViewState extends State<ColorListView> {
+  List<Color> colors = [
+    const Color(0xff717EC3),
+    const Color(0xff496DDB),
+    const Color(0xffC95D63),
+    const Color(0xffB7999C),
+    const Color(0xffAAAAAA),
+    const Color(0xffDFE0E2),
+  ];
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 38 * 2,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 10,
+        itemCount: colors.length,
         itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.only(right: 8),
-            child: ColorItem(),
+          return Padding(
+            padding: const EdgeInsets.only(right: 6),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorItem(
+                color: colors[index],
+                isActive: currentIndex == index,
+              ),
+            ),
           );
         },
       ),
